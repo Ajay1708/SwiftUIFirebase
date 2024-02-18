f2eaa2795ba17a1c4e8ff75cc2a10b2ca8b424c9
Ajay <ajay.nellori@gmail.com>
2024-02-18 21:16:11 +0530

    Initial Commit

---
+++ SwiftUIFirebase/SwiftUIFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseApp.swift
+//  SwiftUIFirebase
+//
+//  Created by Venkata Ajay Sai Nellori on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
